9fac08f467767509fdbe78793e6feed205e5c723
Don Buchanan <mail@donaldbuchanan.com>
2013-07-11 15:40:02 +1000

    return AMEX type with length is 15 and prefix is 34

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -1,2 +1,12 @@
 class CreditCardChecker
+  def initialize(card)
+    @card = card
+  end
+    
+  def type
+    case 
+    when (length == 15 and prefix(2) == "34")
+      "AMEX"
+    end
+  end
 end
--- credit_card_checker_spec.rb
+++ credit_card_checker_spec.rb
@@ -1,4 +1,12 @@
 require_relative 'credit_card_checker'
 
 describe CreditCardChecker do
+  describe "validating type" do
+    it "should find AMEX when starting with 34 and with length of 15" do
+      ccc = CreditCardChecker.new '34344'
+      ccc.stub(:length).and_return(15)
+      ccc.should_receive(:prefix).with(2).and_return("34")
+      ccc.type.should eql "AMEX"
+    end
+  end
 end
