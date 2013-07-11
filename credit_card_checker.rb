2605d7c0b3f081a0fbda931ede347eb0f55ddee2
Don Buchanan <mail@donaldbuchanan.com>
2013-07-11 15:49:21 +1000

    return MasterCard type with length is 16 and prefix is 51-53

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -5,10 +5,12 @@ class CreditCardChecker
     
   def type
     case 
-    when (length == 15 and (prefix(2) == "34" or prefix(2) == "37"))
-      "AMEX"
-    when (length == 16 and prefix(4) == "6011")
-      "Discover"
+    when (length == 15 and (prefix(2) == '34' or prefix(2) == '37'))
+      'AMEX'
+    when (length == 16 and prefix(4) == '6011')
+      'Discover'
+    when (length == 16 and ['51','52','53'].include?(prefix(2)))
+      'MasterCard'
     end
   end
 end
--- credit_card_checker_spec.rb
+++ credit_card_checker_spec.rb
@@ -8,17 +8,30 @@ describe CreditCardChecker do
       ccc.should_receive(:prefix).with(2).and_return("34")
       ccc.type.should eql "AMEX"
     end
+
     it "should find AMEX when starting with 37 and with length of 15" do
       ccc = CreditCardChecker.new '34344'
       ccc.stub(:length).and_return(15)
       ccc.should_receive(:prefix).twice.with(2).and_return("37")
       ccc.type.should eql "AMEX"
     end
+
     it "should find Discover when starting with 6011 and with length of 16" do
       ccc = CreditCardChecker.new '6011'
       ccc.stub(:length).and_return(16)
       ccc.should_receive(:prefix).with(4).and_return("6011")
       ccc.type.should eql "Discover"
     end
+
+    it "should find MasterCard when starting with 51-53 and with length of 16" do
+      ccc = CreditCardChecker.new '533434'
+      ccc.stub(:length).and_return(16)
+      ccc.stub(:prefix).with(4).and_return("324")
+      (51..53).each do |prefix|
+        ccc.should_receive(:prefix).with(2).and_return(prefix.to_s)
+        ccc.type.should eql "MasterCard"
+      end
+    end
+
   end
 end
