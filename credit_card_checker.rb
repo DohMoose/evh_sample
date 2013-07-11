96180101b5fc7cf39d13cc4223ff1e5f85f0bcc0
Don Buchanan <mail@donaldbuchanan.com>
2013-07-11 15:43:15 +1000

    return Discover type with length is 16 and prefix is 6011

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -7,6 +7,8 @@ class CreditCardChecker
     case 
     when (length == 15 and (prefix(2) == "34" or prefix(2) == "37"))
       "AMEX"
+    when (length == 16 and prefix(4) == "6011")
+      "Discover"
     end
   end
 end
--- credit_card_checker_spec.rb
+++ credit_card_checker_spec.rb
@@ -14,5 +14,11 @@ describe CreditCardChecker do
       ccc.should_receive(:prefix).twice.with(2).and_return("37")
       ccc.type.should eql "AMEX"
     end
+    it "should find Discover when starting with 6011 and with length of 16" do
+      ccc = CreditCardChecker.new '6011'
+      ccc.stub(:length).and_return(16)
+      ccc.should_receive(:prefix).with(4).and_return("6011")
+      ccc.type.should eql "Discover"
+    end
   end
 end
